25cb64a9c223c3a8bb89e7af67df1c8bb166a3c4
Carrotz45 <anisah.johansson@gmail.com>
2026-02-06 14:24:58 +0100

    Update request.rb

--- handout/1 - example_requests/lib/request.rb
+++ handout/1 - example_requests/lib/request.rb
@@ -10,18 +10,18 @@ class Request
   def request_arr(string) #make this function more moduable and work if there is more than two headers. Also work let headers have more values (maybe make a two dim array for headers(?) so it kinda works like a table)
     
     input = string.split("\n")
-    @method_resource_version = input[0].split() 
+    method_resource_version = input[0].split() 
     possible_params = []
-    possible_params << @method_resource_version[1]
-    @params_arr = []
-    @header_arr = []
-    @result = []
+    possible_params << method_resource_version[1]
+    params_arr = []
+    header_arr = []
+    result = []
 
     for i in 1..input.length-1 do #ändra till each istället
       content = input[i]
       if content.include?(":") #else identifiera som param om den har ?, ev gör en identifiera param function
         content = content.split(": ")
-        @header_arr << content
+        header_arr << content
       else
         if content.empty? == false
           possible_params << content
@@ -31,25 +31,25 @@ class Request
 
     possible_params.each do |param| #använd .map D:<, GÖR OM ALLT
       temp_param = nil
-      if param.include?("?")
+      if param.include?("?") #kollar om det är en param i request
         _resource, temp_param = param.split("?")
         temp_param = temp_param.split("&").map{|x| x.split("=")}
         temp_param.each do |key_and_value|
-          @params_arr << key_and_value
+          params_arr << key_and_value
         end
-      elsif param.include?("/") == false
+      elsif param.include?("/") == false #kolla efter param i överig text. possible param tar alltid med resource
         temp_param = param
         temp_param = temp_param.split("&").map{|x| x.split("=")}
         temp_param.each do |key_and_value|
-          @params_arr << key_and_value
+          params_arr << key_and_value
         end
       end
     end
 
-    @result = @method_resource_version
-    @result << @header_arr.to_h
-    @result << @params_arr.to_h
-    return @result
+    result = method_resource_version
+    result << header_arr.to_h
+    result << params_arr.to_h
+    return result
 
   end
 end
